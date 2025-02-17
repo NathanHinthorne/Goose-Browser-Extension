5d2b6b7d0e08aa8063364e13ba16191d231f297e
Nathan Hinthorne <nathanrh856@gmail.com>
2025-02-16 18:28:22 -0800

    remove references to DEBUG_MODE

--- background.js
+++ background.js
@@ -1,16 +1,23 @@
+// importScripts('ExtPay.js');
+
+// const extPay = ExtPay('annoying-goose'); 
+// extPay.startBackground();
+
+// extPay.getUser().then(user => {
+// 	console.log(user)`
+// })
+
+
+
 // fires when the extension is first installed or updated
 chrome.runtime.onInstalled.addListener(() => {
-  if (DEBUG_MODE) {
-    console.log('Extension installed');
-  }
+  console.log('Extension installed');
   triggerExtensionStartup();
 });
 
 // fires when entire browser first starts up
 chrome.runtime.onStartup.addListener(() => {
-  if (DEBUG_MODE) {
-    console.log('Browser started');
-  }
+  console.log('Browser started');
   triggerExtensionStartup();
 });
 
@@ -18,27 +25,22 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // React to tab updates
   if (changeInfo.status === 'complete') {
     // Tab finished loading
-    if (DEBUG_MODE) {
-      console.log('Tab loaded:', tab.url);
-    }
+    console.log('Tab loaded:', tab.url);
     triggerExtensionStartup();
   }
 });
 
 function triggerExtensionStartup() {
+  console.log('triggerExtensionStartup() called');
   // Query for all tabs and send message to each
   chrome.tabs.query({}, (tabs) => {
     tabs.forEach(tab => {
       chrome.tabs.sendMessage(tab.id, { command: "extensionStartup" }, (response) => {
         if (chrome.runtime.lastError) {
-          if (DEBUG_MODE) {
-            console.log(`Tab ${tab.id} not ready yet`);
-          }
+          console.log(`Tab ${tab.id} not ready yet`);
           return;
         }
-        if (DEBUG_MODE) {
-          console.log('Extension startup response:', response);
-        }
+        console.log('Extension startup response:', response);
       });
     });
   });
@@ -58,9 +60,7 @@ function clearBadge() {
 
 // Listen for messages from popup
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (DEBUG_MODE) {
-    console.log("background.js recieved message:", message);
-  }
+  console.log("background.js recieved message:", message);
 
   switch( message.action ) {
     case "startTimer":
